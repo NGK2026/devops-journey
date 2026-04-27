86deb3612babf4997b6f4ef536d7633c613b9f94
Nazeeh-K <nazeeh10@hotmail.com>
2026-04-27 23:17:03 +0300

    fix/update projects 01 sh

--- bash/scripts_projects/01-backupTimestamp.sh
+++ bash/scripts_projects/01-backupTimestamp.sh
@@ -2,3 +2,24 @@
 
 # Backs up a directory to another location with a timestamp
 
+TIMESTAMP=$(date +%F)
+SOURCE_FOLDER="/home/student/projects/git/devops-journey/bash/scripts_practice"
+TARGET_FOLDER="/home/student/projects/git/devops-journey/bash/scripts_projects/bak_$TIMESTAMP"
+TARGET_PARENT="/home/student/projects/git/devops-journey/bash/scripts_projects"
+
+FILES2BAK=("$SOURCE_FOLDER"/*)
+
+# create bak dir if not exist
+mkdir -p "$TARGET_FOLDER"
+
+for FILE in "${FILES2BAK[@]}"; do
+    echo -e "\n===** backing up $FILE **==="
+    cp "$FILE" "$TARGET_FOLDER"
+done
+
+echo -e "\n===** zipping backup folder to $TARGET_PARENT **==="
+
+# -c create -z gzip -f next argument is name of file
+tar -czf "$TARGET_PARENT/bak_$TIMESTAMP.tar.gz" "$TARGET_FOLDER"
+
+echo -e "\n===** zipping complete! **===" 
