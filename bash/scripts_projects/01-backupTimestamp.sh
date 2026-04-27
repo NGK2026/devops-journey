2335f770d8f6365446072f6d1db14cbe84e53112
Nazeeh-K <nazeeh10@hotmail.com>
2026-04-27 23:26:06 +0300

    update/fix project 01 sh, add output bak dir, add backup tar

--- bash/scripts_projects/01-backupTimestamp.sh
+++ bash/scripts_projects/01-backupTimestamp.sh
@@ -14,12 +14,35 @@ mkdir -p "$TARGET_FOLDER"
 
 for FILE in "${FILES2BAK[@]}"; do
     echo -e "\n===** backing up $FILE **==="
-    cp "$FILE" "$TARGET_FOLDER"
+    # -n no-clobber (dont overwrite, just skip)
+    cp -n "$FILE" "$TARGET_FOLDER"
 done
 
 echo -e "\n===** zipping backup folder to $TARGET_PARENT **==="
 
-# -c create -z gzip -f next argument is name of file
-tar -czf "$TARGET_PARENT/bak_$TIMESTAMP.tar.gz" "$TARGET_FOLDER"
+# -c create -z gzip -f next argument is name of file -C change DIR
+tar -czf "$TARGET_PARENT/bak_$TIMESTAMP.tar.gz" -C "$TARGET_PARENT" "bak_$TIMESTAMP"
 
 echo -e "\n===** zipping complete! **===" 
+
+: '
+Output:
+
+===** backing up /home/student/projects/git/devops-journey/bash/scripts_practice/01-analyse-logs.sh **===
+
+===** backing up /home/student/projects/git/devops-journey/bash/scripts_practice/02-echo-lyseLogs.sh **===
+
+===** backing up /home/student/projects/git/devops-journey/bash/scripts_practice/03-absolute-lyseLogs.sh **===
+
+===** backing up /home/student/projects/git/devops-journey/bash/scripts_practice/04-variables-lyseLog.sh **===
+
+===** backing up /home/student/projects/git/devops-journey/bash/scripts_practice/05-arrayVariable-lyseLog.sh **===
+
+===** backing up /home/student/projects/git/devops-journey/bash/scripts_practice/06-loop-lyseLogs.sh **===
+
+===** backing up /home/student/projects/git/devops-journey/bash/scripts_practice/07-conditional-lyseLogs.sh **===
+
+===** zipping backup folder to /home/student/projects/git/devops-journey/bash/scripts_projects **===
+
+===** zipping complete! **===
+'
