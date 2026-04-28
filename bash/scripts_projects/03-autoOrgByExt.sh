c0fbf0d7c35868ce62ac954213af69e9d458a551
Nazeeh-K <nazeeh10@hotmail.com>
2026-04-28 10:48:43 +0300

    update proj 03 sh, create ext_folder with ext files

--- bash/scripts_projects/03-autoOrgByExt.sh
+++ bash/scripts_projects/03-autoOrgByExt.sh
@@ -5,3 +5,7 @@
 TARGET_FOLDER="/home/student/projects/git/devops-journey/bash/scripts_projects/ext_folder"
 mkdir -p "$TARGET_FOLDER"
 
+# save extensions in array
+EXT_ARRAY=()
+for FILE in $TARGET_FOLDER/*; do
+    echo "$(FILE | tr -d '.' | awk '{print $2}')"
