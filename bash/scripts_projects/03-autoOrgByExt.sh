e46c9eb6494fc88e5ed77ebc7693f91db5282717
Nazeeh-K <nazeeh10@hotmail.com>
2026-04-28 10:41:56 +0300

    create script project 03 sh

---
+++ bash/scripts_projects/03-autoOrgByExt.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Auto-organizes files in a folder by extension into subfolders
+
+TARGET_FOLDER="/home/student/projects/git/devops-journey/bash/scripts_projects/ext_folder"
+mkdir -p "$TARGET_FOLDER"
+
