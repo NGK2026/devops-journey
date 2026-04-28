fc8f389923a529247a7af71dacbb83058e72cc57
Nazeeh-K <nazeeh10@hotmail.com>
2026-04-28 11:35:49 +0300

    work logic

--- bash/scripts_projects/03-autoOrgByExt.sh
+++ bash/scripts_projects/03-autoOrgByExt.sh
@@ -5,19 +5,32 @@
 TARGET_FOLDER="/home/student/projects/git/devops-journey/bash/scripts_projects/ext_folder"
 mkdir -p "$TARGET_FOLDER"
 
-# save extensions in array
+# save file data in array
+FULL_NAME_ARRAY=()
+NAME_ARRAY=()
 EXT_ARRAY=()
+
 for FILE in "$TARGET_FOLDER"/*; do
     FULL_NAME=$(echo "$FILE" | awk -F/ '{print $NF}')
     FILE_NAME=$(echo "$FILE" | awk -F/ '{print $NF}' | awk -F. '{print $1}')
     EXT=$(echo "$FILE" | awk -F. '{print $2}')
+    
     # echo "$EXT"
+    FULL_NAME_ARRAY+=("$FULL_NAME")
+    NAME_ARRAY+=("$FILE_NAME")
     EXT_ARRAY+=("$EXT")
     echo "File: $FULL_NAME | File Name: $FILE_NAME | Extension: $EXT"
 done
 
+# create folders
 for EXT in ${EXT_ARRAY[@]}; do
-    echo "$EXT"
+    # echo "$EXT"
     mkdir -p "$TARGET_FOLDER"/"$EXT"
-    echo "created $TARGET_FOLDER/$EXT"
+    FOLDER=$(echo "$TARGET_FOLDER/$EXT" | awk -F/ '{print $(NF-1) "/" $NF}')
+    echo "$FOLDER"
 done
+
+# move files in folders respectively 
+# for FILE in ${FULL_NAME_ARRAY[@]}; do
+    
+    
