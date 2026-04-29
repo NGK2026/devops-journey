706bd29f7ee967e7a0019ffde0a3779d036b2b4b
Nazeeh-K <nazeeh10@hotmail.com>
2026-04-29 20:40:31 +0300

    update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,9 @@ provider "aws" {
   secret_key = "--snip--"
 }
 
+# create instance called my-server with 
+# specific AMI ID of Amazon Linux kernel 6.1 
+# instance type t3.micro
 resource "aws_instance" "my-server" {
   ami           = "ami-0a0823e4ea064404d" # name of instance (AMI ID)
   instance_type = "t3.micro"
