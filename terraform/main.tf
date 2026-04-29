573bf1543159801ee96cd22ac2ea82dd1327145c
Nazeeh-K <nazeeh10@hotmail.com>
2026-04-29 21:55:24 +0300

    prettify format

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,7 @@ provider "aws" {
 resource "aws_instance" "my-server" {
   ami           = "ami-0a0823e4ea064404d" # name of instance (AMI ID)
   instance_type = "t3.micro"
+  
   tags = {
     Name = "HelloWorld"
   }
