37332b9108950e2ec1d873fd439fa7c4eeebcb73
Nazeeh-K <nazeeh10@hotmail.com>
2026-04-30 22:21:06 +0300

    modify instance

--- terraform/projects/aws-website-infra/main.tf
+++ terraform/projects/aws-website-infra/main.tf
@@ -120,3 +120,18 @@ resource "aws_eip" "one" {
 }
 
 # 9- Create Amazon Linux server and install/enable apache2
+resource "aws_instance" "web-server-instance" {
+  ami           = "ami-0a0823e4ea064404d" # name of instance (AMI ID)
+  instance_type = "t3.micro"
+  availability_zone = "eu-north-1a"
+  key_name = "main-key"
+  # network interface inside instance is depreciated 
+  # reference with primary_netowrk_interface instead
+  primary_network_interface {
+    network_interface_id = aws_network_interface-web-server-nic.id
+  }
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
