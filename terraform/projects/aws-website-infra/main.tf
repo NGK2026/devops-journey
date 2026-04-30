2c3fdf3a3b55af5adfc2acc6b4f395829c3162e4
Nazeeh-K <nazeeh10@hotmail.com>
2026-04-30 20:31:12 +0300

    modify route table

--- terraform/projects/aws-website-infra/main.tf
+++ terraform/projects/aws-website-infra/main.tf
@@ -18,21 +18,21 @@ resource "aws_internet_gateway" "gw" {
 }
 
 # 3- Create Custom Route Table
-resource "aws_route_table" "example" {
-  vpc_id = aws_vpc.example.id
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
 
   route {
-    cidr_block = "10.0.1.0/24"
-    gateway_id = aws_internet_gateway.example.id
+    cidr_block = "0.0.0.0/0" # default gateway route
+    gateway_id = aws_internet_gateway.gw.id
   }
 
   route {
-    ipv6_cidr_block        = "::/0"
-    egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
+    ipv6_cidr_block        = "::/0" # default gateway route
+    egress_only_gateway_id = aws_internet_gateway.gw.id
   }
 
   tags = {
-    Name = "example"
+    Name = "Prod"
   }
 }
 
