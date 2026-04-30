de888102c4dae939d4c29d7fc5b545cf456080ca
Nazeeh-K <nazeeh10@hotmail.com>
2026-04-30 10:28:21 +0300

    Add Tag to VPC

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,10 @@ provider "aws" {
 #   }
 # }
 
-# Create VPC 
+# Create VPC, and add Tags 
 resource "aws_vpc" "my-vpc" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "production"
+  }
 }
