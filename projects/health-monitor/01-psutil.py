1e727d11ec275c3f4d0e2f93353fd6859da5eb34
Nazeeh-K <nazeeh10@hotmail.com>
2026-05-06 23:24:58 +0300

    get mem

--- projects/health-monitor/01-psutil.py
+++ projects/health-monitor/01-psutil.py
@@ -3,15 +3,38 @@
 
 import psutil as ps
 
+## CPU
 cpuCount = ps.cpu_count()
 nonLogicalCpu = ps.cpu_count(logical=False)
+cpuPercent = ps.cpu_percent(interval=1)
 
 print(
-    f"CPU logical cores: {cpuCount} threads.\n"
-    f"CPU actual physical cores: {nonLogicalCpu} cores.\n"
+    f"\n===== ***** CPU ***** =====\n"
+    f"CPU actual physical cores: {nonLogicalCpu} cores\n"
+    f"CPU logical cores: {cpuCount} threads\n"
+    f"CPU currently at: {cpuPercent} now"
 )
 
 # get cpu usage percent
-for x in range(5):
-    print(f"Currently {ps.cpu_percent(interval=1)}% used")
+# for x in range(5):
+    # print(f"Currently {ps.cpu_percent(interval=1)}% used")
 
+
+## MEMORY
+currentMem = ps.virtual_memory().percent
+usedMem = ps.virtual_memory().used
+freeMem = ps.virtual_memory().free
+totalMem = ps.virtual_memory().total
+totalGB = totalMem / (1024**3)
+usedGB = usedMem / (1024**3)
+freeGB = freeMem / (1024**3)
+print(
+    f"\n===== ***** MEMORY ***** =====\n"
+    f"RAM total: {totalGB:.2f} GB\n"
+    f"RAM used: {usedGB:.2f} GB\n"
+    f"RAM free: {freeGB:.2f} GB\n"
+    f"RAM usage currently at: {currentMem}%\n"
+    f"RAM: {usedGB:.2f} GB / {totalGB:.2f} GB ({currentMem}%)\n"
+)
+
+## DISK
