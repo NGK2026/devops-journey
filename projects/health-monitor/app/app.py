c06975375acda7220531b4c74771d8e786becbf6
Nazeeh-K <nazeeh10@hotmail.com>
2026-05-07 00:41:10 +0300

    add comments

--- projects/health-monitor/app/app.py
+++ projects/health-monitor/app/app.py
@@ -1,4 +1,4 @@
-# Boiler plate
+# uses HTTP to return system metrics gathered by process utility library
 from flask import Flask, jsonify
 import psutil as ps
 
@@ -6,17 +6,21 @@ app = Flask(__name__)
 
 @app.route('/metrics')
 def metrics():
+    # variables storing metrics:
+    # CPU, RAM, DISK (ROOT, HOME)
     cpuPercent = ps.cpu_percent(interval=1)
     memCurr = ps.virtual_memory().percent
     rootDiskCurr = ps.disk_usage('/').percent
     homeDiskCurr = ps.disk_usage('/home').percent
 
+    # Store data in dictionary
     data = {
         'CPU': cpuPercent,
         'RAM': memCurr,
         'Root': rootDiskCurr,
         'Home': homeDiskCurr
     }
+    # return data as JSON
     return jsonify(data)
 
 if __name__ == '__main__':
