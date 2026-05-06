b9bbd97a5d8b88dfb4c647768f91ddbf348d4f16
Nazeeh-K <nazeeh10@hotmail.com>
2026-05-06 23:04:36 +0300

    get cpu data

--- projects/health-monitor/01-psutil.py
+++ projects/health-monitor/01-psutil.py
@@ -1,5 +1,17 @@
 # practice and explore file
 # for cpu, memory and disk
 
-import psutil
+import psutil as ps
+
+cpuCount = ps.cpu_count()
+nonLogicalCpu = ps.cpu_count(logical=False)
+
+print(
+    f"CPU logical cores: {cpuCount} threads.\n"
+    f"CPU actual physical cores: {nonLogicalCpu} cores.\n"
+)
+
+# get cpu usage percent
+for x in range(5):
+    print(f"Currently {ps.cpu_percent(interval=1)}% used")
 
