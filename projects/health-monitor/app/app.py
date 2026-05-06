58b3bd93c5899e1e906bbc50e444d869c57d0d60
Nazeeh-K <nazeeh10@hotmail.com>
2026-05-06 23:57:24 +0300

    jsonify data, fix run app

--- projects/health-monitor/app/app.py
+++ projects/health-monitor/app/app.py
@@ -1,10 +1,23 @@
 # Boiler plate
-from flask import Flask
+from flask import Flask, jsonify
+import psutil as ps
+
 app = Flask(__name__)
 
 @app.route('/metrics')
 def metrics():
-    # psutils
-    return "your data here"
+    cpuPercent = ps.cpu_percent(interval=1)
+    memCurr = ps.virtual_memory().percent
+    rootDiskCurr = ps.disk_usage('/').percent
+    homeDiskCurr = ps.disk_usage('/home').percent
 
-app.run(host='0.0.0.0', port=5000)
+    data = {
+        'CPU': cpuPercent,
+        'RAM': memCurr,
+        'Root': rootDiskCurr,
+        'Home': homeDiskCurr
+    }
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
