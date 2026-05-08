9a342fa7c9ec8e56a8d710feb45f5e6cec5ac3ea
Nazeeh-K <nazeeh10@hotmail.com>
2026-05-08 11:11:05 +0300

    add comments

--- projects/health-monitor/app/app.py
+++ projects/health-monitor/app/app.py
@@ -1,11 +1,12 @@
 # uses HTTP to return system metrics gathered by process utility library
+# use prometheus client to format collected data as prometheus ready
 from flask import Flask, jsonify
 import psutil as ps
 import prometheus_client as pc
 
 app = Flask(__name__)
 
-# define gauge
+# define gauge 'prometheus_client approach'
 cpu_gauge = pc.Gauge('cpu_percent', 'Current CPU percent')
 mem_gauge = pc.Gauge('mem_percent', 'Current RAM percent')
 root_gauge = pc.Gauge('root_percent', 'Current Root percent')
@@ -13,18 +14,14 @@ home_gauge = pc.Gauge('home_percent', 'Current Home percent')
 
 @app.route('/metrics')
 def metrics():
+    ## JSON approach:
+
     # variables storing metrics:
     # CPU, RAM, DISK (ROOT, HOME)
     # cpuPercent = ps.cpu_percent(interval=1)
     # memCurr = ps.virtual_memory().percent
     # rootDiskCurr = ps.disk_usage('/').percent
     # homeDiskCurr = ps.disk_usage('/home').percent
-    
-    # set gauge
-    cpu_gauge.set(ps.cpu_percent(interval=1))
-    mem_gauge.set(ps.virtual_memory().percent)
-    root_gauge.set(ps.disk_usage('/').percent)
-    home_gauge.set(ps.disk_usage('/home').percent)
 
     # Store data in dictionary
     # data = {
@@ -33,7 +30,15 @@ def metrics():
     #     'Root': rootDiskCurr,
     #     'Home': homeDiskCurr
     # }
-    # return latest gauge          
+    # return jsonify(data)    
+    
+    # set gauge 
+    cpu_gauge.set(ps.cpu_percent(interval=1))
+    mem_gauge.set(ps.virtual_memory().percent)
+    root_gauge.set(ps.disk_usage('/').percent)
+    home_gauge.set(ps.disk_usage('/home').percent)
+
+    # set correct content type
     return pc.generate_latest(), 200, {'Content-Type': pc.CONTENT_TYPE_LATEST} 
 
 if __name__ == '__main__':
