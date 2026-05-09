8939a89e08647dad3ecac0e2287e6b9ce3d890aa
Nazeeh-K <nazeeh10@hotmail.com>
2026-05-09 11:12:14 +0300

    add todos

---
+++ projects/health-monitor/02-health-service.sh
@@ -0,0 +1,11 @@
+!#/bin/bash
+
+# TODO: check if container is running by filter
+
+
+# TODO: if not running check if exists but stopped or
+# not exist at all
+
+# TODO: if exists: start
+
+# TODO: if not exist: run
