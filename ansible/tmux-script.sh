bd97b2bfb0e64d92111ea7a6a730f1f968b921e2
Nazeeh-K <nazeeh10@hotmail.com>
2026-05-17 01:32:38 +0300

    add centos

--- ansible/tmux-script.sh
+++ ansible/tmux-script.sh
@@ -10,12 +10,14 @@ tmux new-window -t $SESSION:1 -n 'srv1'
 tmux new-window -t $SESSION:2 -n 'srv2'
 tmux new-window -t $SESSION:3 -n 'srv3'
 tmux new-window -t $SESSION:4 -n 'srv4'
+tmux new-window -t $SESSION:4 -n 'srv5'
 
 # Optional: Send SSH commands to the server windows (ctrl + m) = 'Enter'
 tmux send-keys -t $SESSION:1 'ssh 192.168.0.124' C-m 
 tmux send-keys -t $SESSION:2 'ssh 192.168.0.139' C-m 
 tmux send-keys -t $SESSION:3 'ssh 192.168.0.3' C-m
 tmux send-keys -t $SESSION:4 'ssh 192.168.0.66' C-m
+tmux send-keys -t $SESSION:4 'ssh 192.168.0.171' C-m
 
 # Select the first window and attach
 tmux select-window -t $SESSION:0
