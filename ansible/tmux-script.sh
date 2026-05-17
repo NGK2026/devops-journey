6f5e05aa628f718559abfc3bad9b410250e5ca82
Nazeeh-K <nazeeh10@hotmail.com>
2026-05-17 03:15:55 +0300

    update srvr list

--- ansible/tmux-script.sh
+++ ansible/tmux-script.sh
@@ -8,14 +8,18 @@ tmux new-session -d -s $SESSION -n 'main'
 # Create the server windows
 tmux new-window -t $SESSION:1 -n 'arch-srv1'
 tmux new-window -t $SESSION:2 -n 'ubu-22-srv2'
-tmux new-window -t $SESSION:3 -n 'ubu-26-srv3'
-tmux new-window -t $SESSION:4 -n 'cent-srv4'
+tmux new-window -t $SESSION:3 -n 'ubu-22-srv3'
+tmux new-window -t $SESSION:4 -n 'ubu-26-srv4'
+tmux new-window -t $SESSION:5 -n 'ubu-26-srv5'
+tmux new-window -t $SESSION:6 -n 'cent-srv6'
 
 # Optional: Send SSH commands to the server windows (ctrl + m) = 'Enter'
 tmux send-keys -t $SESSION:1 'ssh 192.168.0.38' C-m
 tmux send-keys -t $SESSION:2 'ssh 192.168.0.124' C-m 
-tmux send-keys -t $SESSION:3 'ssh 192.168.0.3' C-m
-tmux send-keys -t $SESSION:4 'ssh 192.168.0.171' C-m
+tmux send-keys -t $SESSION:3 'ssh 192.168.0.139' C-m 
+tmux send-keys -t $SESSION:4 'ssh 192.168.0.3' C-m
+tmux send-keys -t $SESSION:5 'ssh 192.168.0.66' C-m
+tmux send-keys -t $SESSION:6 'ssh 192.168.0.171' C-m
 
 # Select the first window and attach
 tmux select-window -t $SESSION:0
