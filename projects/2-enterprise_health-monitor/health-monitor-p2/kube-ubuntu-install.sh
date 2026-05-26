5d6f506d3865a1ec974bcc39722fdf7a5e4ef7db
Nazeeh-K <nazeeh10@hotmail.com>
2026-05-26 17:41:34 +0300

    add install kube instructions

--- projects/2-enterprise_health-monitor/health-monitor-p2/kube-ubuntu-install.sh
+++ projects/2-enterprise_health-monitor/health-monitor-p2/kube-ubuntu-install.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 # install kubernetes on ubuntu
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.36/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.36/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+sudo systemctl enable --now kubelet
