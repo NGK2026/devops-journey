43683541b644feb4000cd045eae773bfe7372dc5
Nazeeh-K <nazeeh10@hotmail.com>
2026-05-26 18:15:30 +0300

    add install commands

--- projects/2-enterprise_health-monitor/files/kube-centos-install.sh
+++ projects/2-enterprise_health-monitor/files/kube-centos-install.sh
@@ -2,3 +2,19 @@
 
 # install kube on centos
 
+setenforce 0
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+cat <<EOF | tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.36/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.36/rpm/repodata/repomd.xml.key
+exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
+EOF
+
+yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+yum install -y kubelet kubeadm kubectl --setopt=disable_excludes=kubernetes
+systemctl enable --now kubelet
