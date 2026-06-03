9ef256dfecb8db5517a6c358705305d6dec0eff6
Nazeeh-K <nazeeh10@hotmail.com>
2026-06-03 11:24:16 +0300

    modify

--- linux(LFCS)/bash/patterns.sh
+++ linux(LFCS)/bash/patterns.sh
@@ -2,10 +2,10 @@
 
 # to test pattern match, use /usr/bin/hello
 
-filename=${1##*/}
-echo 'filename=${1##*/}'
+filename=${1#*/}
+echo 'filename=${1#*/}'
 echo "The name of the file is $filename"
 
-directoryname=${1%/*}
-echo 'directoryname=${1%/*}'
+directoryname=${1%%/*}
+echo 'directoryname=${1%%/*}'
 echo "the name of the directory is $directoryname"
