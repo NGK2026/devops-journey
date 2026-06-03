88bffde694ca9fa89bcd5b0d6c0c437825af095f
Nazeeh-K <nazeeh10@hotmail.com>
2026-06-03 11:10:47 +0300

    add patternmatch

---
+++ linux(LFCS)/bash/patterns.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# to test pattern match, use /usr/bin/hello
+
+filename=${1##*/}
+echo 'filename=${1##*/}'
+echo "The name of the file is $filename"
+
+directoryname=${1%/*}
+echo 'directoryname=${1%/*}'
+echo "the name of the directory is $directoryname"
