00958817e20708326975ea22d5600e9e350ebf6a
Nazeeh-K <nazeeh10@hotmail.com>
2026-06-09 23:35:35 +0300

    add read file, match pattern

--- linux(LFCS)/bash/labs/1-lab_1.sh
+++ linux(LFCS)/bash/labs/1-lab_1.sh
@@ -6,11 +6,20 @@ Extract username only and write them to a new file.
 Based on new file, create local user accounts for each user.
 END_COMMENT
 
+# Test sample
 EXAMPLE="cn=lisa17,dc=example,dc=com"
 
 echo $EXAMPLE
 
+# Pattern match, remove longest match after comma " , "
 EXAMPLE=${EXAMPLE%%,*}
 
 echo ${EXAMPLE#cn=}
 
+# read file line by line with while loop
+while read line; do
+	echo "${line%%,*}" >> 1-user_output.txt
+done < 1-ldapusers.txt
+
+# print user_output.txt
+cat 1-user_output.txt
